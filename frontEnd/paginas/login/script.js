8acc30785d1da3a3f3ff1cca684f68a4b73239fa
unknown <TrenksSS>
2022-11-25 11:32:51 -0300

    Forum att1

--- frontEnd/paginas/login/script.js
+++ frontEnd/paginas/login/script.js
@@ -1,5 +1,7 @@
 var olho = document.querySelector("#senha-login-user")
 var img = document.querySelector("#olho")
+var cad = document.querySelector(".modal-cadastro")
+var log = document.querySelector(".modal-login")
 function ver(){
     if(olho.type == "password"){
         olho.type = "text" 
@@ -9,3 +11,12 @@ function ver(){
         img.src = "../../../assets/olho.png"
     }
 }
+ function cadastrar() {
+    log.style = "display:none"
+    cad.style = "display:flex"
+ }
+
+ function voltaLog() {
+    log.style = "display:flex"
+    cad.style = "display:none"
+ }
