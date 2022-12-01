701caf2c9fe697e7edcba5f19238dcbacb0a7707
unknown <TrenksSS>
2022-12-01 11:34:08 -0300

    Prava luana problema com login no pc dela

---
+++ frontEnd/paginas/home/script.js
@@ -0,0 +1,13 @@
+var post = document.querySelector(".info")
+var x = document.querySelector("#x")
+
+function abrir() {
+    post.classList.add("teste")
+    //  post.style = "display:flex"
+    //  post.style = "opacity:1"
+}
+function fechar() {
+    console.log("ok")
+ post.style = "display:none"
+//  post.style = "opacity:0"
+}
