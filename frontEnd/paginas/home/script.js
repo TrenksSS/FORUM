b580f3ecd37adb557271a9f1aaaf47aad6ad2a83
unknown <TrenksSS>
2022-12-02 10:32:38 -0300

    Forum pg 2

--- frontEnd/paginas/home/script.js
+++ frontEnd/paginas/home/script.js
@@ -1,19 +1,32 @@
 var post = document.querySelector(".info")
 var x = document.querySelector("#x")
 var cora = document.querySelector("#like")
+var np = document.querySelector("#novo-post")
+
 
 function abrir() {
-     post.style = "display:flex"
+    post.style = "display:flex"
 }
 function fechar() {
- post.style = "display:none"
+    post.style = "display:none"
 }
 
 function like() {
     console.log("ok")
-    if(cora.src == "../../../assets/coracaoN.png"){
+    if (cora.src == "../../../assets/coracaoN.png") {
         cora.src = "../../../assets/coracao.png"
-    }else{
+    } else {
         cora.src = "../../../assets/coracaoN.png"
     }
+}
+
+function newpost() {
+    np.style = "font-size:20px"
+    setTimeout(() => {np.innerHTML = "New Post +"}, 350);
+    np.style.transition = "0.5s"
+}
+function newp() {
+    np.innerHTML = "+"
+    np.style.width = "1px"
+    np.style.transition = "1s"
 }
